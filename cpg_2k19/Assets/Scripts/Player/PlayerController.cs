18839a4d8b291d57817f9895d045e2bebcc00241
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2019-05-18 05:55:26 -0300

    [ADD] Player 8 axis animation

--- cpg_2k19/Assets/Scripts/Player/PlayerController.cs
+++ cpg_2k19/Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    #region Variables
+
+    private bool movementSet = false;
+
+    #endregion
+
+    private void SetAnimatorBool(Animator animator, string name, bool value)
+    {
+        if (!movementSet)
+        {
+            animator.SetBool(name, value);
+            movementSet = true;
+        }
+    }
+
     public Vector3 GetMovementVector(float XAxis, float YAxis)
     {
         Vector3 _out = new Vector3(XAxis, YAxis);
@@ -19,26 +34,47 @@ public class PlayerController : MonoBehaviour
     {
         Player player = GlobalVariables.player;
         Animator animator = player.animator;
+        SpriteRenderer spriteRenderer = player.GetComponent<SpriteRenderer>();
+
 
         // Captures user's input
         float axisY = Input.GetAxis("Vertical");
         float axisX = Input.GetAxis("Horizontal");
 
+        // Player's movement vector
+        Vector3 movement = GetMovementVector(axisX, axisY) * player.speed;
+
+        // Diagonal Movement
+        if (axisX != 0 && axisY > 0.2f)
+        {
+            animator.SetBool("diag_up", true);
+            animator.SetBool("diag_down", false);
+        }
+        else if (axisX != 0 && axisY < -0.2f)
+        {
+            animator.SetBool("diag_up", false);
+            animator.SetBool("diag_down", true);
+        }
+        else
+        {
+            animator.SetBool("diag_up", false);
+            animator.SetBool("diag_down", false);
+        }
 
         // Horizontal Movement
-        if(axisX == 0)
+        if (axisX > 0)
         {
-            animator.SetBool("horizontal", false);
+            spriteRenderer.flipX = false;
+            animator.SetBool("horizontal", true);
         }
-        else if(axisX > 0)
+        else if (axisX < 0)
         {
-            player.GetComponent<SpriteRenderer>().flipX = false;
+            spriteRenderer.flipX = true;
             animator.SetBool("horizontal", true);
         }
         else
         {
-            player.GetComponent<SpriteRenderer>().flipX = true;
-            animator.SetBool("horizontal", true);
+            animator.SetBool("horizontal", false);
         }
 
         // Vertical Movement
@@ -56,16 +92,15 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("down", true);
         }
 
-        // Player's movement vector
-        Vector3 movement = GetMovementVector(axisX, axisY) * player.speed;
-
         // Move the player
         transform.Translate(movement * Time.deltaTime, Space.World);
+
+        movementSet = false;
     }
 
     private void Update()
     {
-        HandleInput();    
+        HandleInput();
     }
 
 }
