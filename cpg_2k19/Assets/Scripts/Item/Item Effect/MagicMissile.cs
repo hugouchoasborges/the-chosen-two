8dd9768925e3d6c33c4b141a04716150c4e06b5a
NixonMSilva <nixonmoreira@hotmail.com>
2019-05-19 05:14:12 -0300

    [ADD] Magic Missile Implementation

--- cpg_2k19/Assets/Scripts/Item/Item Effect/MagicMissile.cs
+++ cpg_2k19/Assets/Scripts/Item/Item Effect/MagicMissile.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EffectSwordSwing : Projectiles
+public class MagicMissile : MonoBehaviour
 {
+    SphereCollider missileCollider;
+    Rigidbody2D missileBody;
+
     // Start is called before the first frame update
     void Start()
     {
-  
+        missileCollider.GetComponent<SphereCollider>();
+        missileBody.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
---
+++ cpg_2k19/Assets/Scripts/Item/Item Types/ItemMagic.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMagic : Item
+{
+    public Rigidbody2D magicMissile;
+    public float magicMissileSpeed = 8f;
+    public GameObject magicCaster;
+
+    public override void useItem(GameObject user)
+    {
+        executeMagicStrike(user);
+        // Destroy the item if its health drops to 0
+        --itemHealth;
+        //Debug.Log("Used a barrier item! Its health is now " + itemHealth);
+    }
+
+    public override void dropItem()
+    {
+
+    }
+
+    void executeMagicStrike(GameObject user)
+    {
+        Debug.Log("Usou Mágica");
+        GameObject enemy;
+        // Instantiate the missle pointed towards the hostile
+
+        if (user.name == "Player")
+        {
+            enemy = GameObject.Find("Player2");
+        }
+        else
+        {
+            enemy = GameObject.Find("Player");
+        }        
+        Vector2 enemyPos = enemy.transform.position;
+        Vector2 userPos = user.transform.position;
+        Vector2 angle = enemyPos - userPos;
+        Rigidbody2D missile = Instantiate(magicMissile, user.transform.position, Quaternion.identity);
+        missile.velocity = angle;
+        Physics.IgnoreCollision(missile.gameObject.GetComponent<SphereCollider>(), user.GetComponent<BoxCollider>());
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
